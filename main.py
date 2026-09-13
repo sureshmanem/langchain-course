ea19337d3510b480cbc32e97e31963b4ff9a3f79
Suresh Manem <16394252+sureshmanem@users.noreply.github.com>
2026-09-13 16:36:18 -0400

    Environment Setup

---
+++ main.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+import os
+
+from langchain_openai import ChatOpenAI
+
+
+def main():
+    print("Hello from langchain-course!")
+
+
+if __name__ == "__main__":
+    main()
